39ff3d2ac54dc8ae3334fdffe14cbdb4f0c8d6f6
Dandy Arif Rahman <dandyarifrahman@gmail.com>
2019-01-24 09:11:35 +0700

    first commit

---
+++ Muhandis/admin1/muhandis/pendaftaran/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="./css/style.css">	
+	<link rel="stylesheet" href="./css/w3.css">	
+    <title>
+        Pendaftaran Ma`had Al-Muhandis
+    </title>
+    <style>
+        p{
+           font-size:30px;
+           font-family:sans-serif;
+           color: white;
+        }
+    </style>
+</head>
+
+
+<body id="body">
+<div id="wrapper">
+    <div class="penjelasan">
+        <center><br>
+            <a href="https://muhandis.udrussunnah.org" title="Ma`had Al-Muhandis" target="_blank">
+            <img width="200px" src="./pic/logo.png" alt=""/>
+            </a>
+        </center>
+    </div>
+    
+    <div>
+        <center><hr>
+            <img width="50%" src="./pic/ditutup.jpg" /><br><br>
+            <p>Pendaftaran akan dibuka lagi pada tahun ajaran baru, InsyaaAllaah</p>
+            <br><br><hr><br><br><br><br><br><br><br><br><br><br>
+        </center>
+    </div>
+</div>
+</body>
+</html>
---
+++ Muhandis/admin1/taysir/pendaftaran/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="./css/style1.css">	
+	<link rel="stylesheet" href="./css/w3.css">	
+    <title>
+        Pendaftaran Ma`had At-Taysir
+    </title>
+</head>
+    <body id="body">
+        <div id="wrapper" class="height1">
+        <div class=" height1 w3-card-4">
+            <div class="w3-container blue">
+                        <center>
+                            <br>
+                            <a href="https://taysir.udrussunnah.org" title="Ma`had At-Taysir" target="_blank">
+                            <img width="250px" src="./pic/logo.png" alt="Logo Muhandis">
+                            </a>
+                        </center>
+                        <center><hr>
+                            <h1>Pendaftaran Program Bahasa Arab Ma`had At-Taysir<br>
+                                Semester 2 Tahun Ajaran 1439/1440 H</h1>
+                            <hr>
+                        </center>
+            </div>
+
+            <form class="height w3-container" action="umum.php" method="POST" name="">
+                <div class="height">
+                    <center>
+                    <h1>Pendaftaran Telah Ditutup</h1>
+                    <h2>Pendaftaran akan dibuka lagi pada tahun ajaran baru, InsyaaAllaah</h2>
+                    </center>
+                </div>
+            </form>
+
+                
+            <a class="" href="https://taysir.udrussunnah.org">
+            <div class="w3-container w3-card-4 kembali blue w3-btn">
+                    <p>Halaman Utama</p>
+            </div>
+            </a>
+        </div>
+        </div>
+
+       
+
+    </body>
+</html>
